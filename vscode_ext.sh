baed02993029d06646f27cd041bd1bc1dd534a72
Dylan Maccora <maccora17@gmail.com>
2019-02-11 16:54:06 +1100

    Complete vscode extension script

--- vscode_ext.sh
+++ vscode_ext.sh
@@ -2,15 +2,9 @@
 function install {
     code --install-extension $1
 }
-install alefragnani.project-manager
-install eamodio.gitlens
-install xabikos.reactsnippets
-install yzhang.markdown-all-in-one
-install robertohuertasm.vscode-icons
-install monokai.theme-monokai-pro-vscode
-install infeng.vscode-react-typescript
-install editorconfig.editorconfig
-install whizkydee.material-palenight-theme
-install stkb.rewrap
-install rust-lang.rust
-install wix.vscode-import-cost
+
+filename="vscode/extensions.txt"
+while read -r line
+do
+    install $line
+done < "$filename"
