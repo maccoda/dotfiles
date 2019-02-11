b858b7b0e3914aa35ceebd1d3933a8e84270cd61
Dylan Maccora <maccora17@gmail.com>
2019-02-11 21:11:37 +1100

    Completed linux set up

--- linux_bootstrap.sh
+++ linux_bootstrap.sh
@@ -18,6 +18,9 @@ sudo apt install code
 
 echo "Add some fonts"
 sudo apt install fonts-firacode
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+sudo apt-get install guake
 
 echo "Set up the theme"
 sudo add-apt-repository -u ppa:snwh/ppa
@@ -26,6 +29,8 @@ sudo apt-get install paper-icon-theme
 echo "Now some useful tools"
 sudo apt-get install gnome-tweak-tool
 sudo apt-get install synapse
+sudo apt-get install docker
+sudo apt-get install docker-compose
 
 echo "Here comes the terminal"
 sudo apt install zsh
