cd0ea2190512df3e1061a856e7cb64189b860e13
Dylan Maccora <maccora17@gmail.com>
2019-06-08 23:26:21 -0700

    Add mongodb start alias

--- fish-conf/alias.fish
+++ fish-conf/alias.fish
@@ -1,5 +1,6 @@
 alias dc="docker-compose"
 alias dots="code $HOME/.dotfiles"
+alias mongo_start='sudo service mongodb start'
 
 # git
 alias grg='git log --pretty=format:"%h%x09%an%x09%ad%x09%s" --date=short'
