3091dd12c41da22e4ed58dd6bc740159bca1288a
Dylan Maccora <maccora17@gmail.com>
2019-10-13 15:38:20 -0700

    Add some new casks

--- fish-conf/alias.fish
+++ fish-conf/alias.fish
@@ -1,8 +1,13 @@
 alias dc="docker-compose"
 alias dots="code $HOME/.dotfiles"
 alias mongo_start='sudo service mongodb start'
+alias postgres_start='sudo service postgresql start'
 
 # git
 alias grg='git log --pretty=format:"%h%x09%an%x09%ad%x09%s" --date=short'
 alias gst='git status'
 alias gd='git diff'
+
+# directory shortcut
+alias dev='cd /mnt/c/Users/dylan/dev'
+
