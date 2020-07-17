157b1a3b005de2a69084f30fcce5ca8142e133a5
Dylan Maccora <dmaccora@amazon.com>
2020-07-17 16:20:30 -0700

    Improve commit search function

--- README.md
+++ README.md
@@ -20,4 +20,11 @@ Usage:
 > ./install
 ```
 
+## iTerm2 Setup
 
+iTerm2 does not have a nice configuration I can add here so I will just note it
+down.
+
+Color: Flatland
+Font: FiraCode Nerd Font (add ligatures)
+Command: fish
--- fish-conf/function.fish
+++ fish-conf/function.fish
@@ -1,6 +1,10 @@
 # Usage: Search across commit messages for the given search term
 function commit
-    grg | rg $argv[1]
+    if $argv[1] == "--show"
+        grg | rg $argv[2] | cut -f 1 | xargs git show
+    else
+        grg | rg $argv[1]
+    end
 end
 
 #Usage: squash N where N is number of commits to squash
