3497ef711fe10e05060a1591fc75ea189fc860eb
Dylan Maccora <dmaccora@amazon.com>
2020-08-21 17:07:36 -0700

    Speed up shell by removing fisher check

--- fish-conf/config.fish
+++ fish-conf/config.fish
@@ -5,13 +5,6 @@ set -gx FZF_DEFAULT_COMMAND 'fd --type f'
 # For linux /home/linuxbrew/.linuxbrew/bin
 set -gx PATH /home/linuxbrew/.linuxbrew/bin $PATH
 
-# Bootstrap fisher
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
-
 # Remove the welcome message
 set fish_greeting
 
