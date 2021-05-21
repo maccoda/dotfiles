5a5b830b4b75af72b5e10d81ebfdc0dd974abd11
maccoda <maccora17@gmail.com>
2021-05-21 13:58:04 -0700

    Add a tmux start function
    This function just simply allows for an easy consistent tmux start up.
    It is far from perfect at the moment but it is a starting point that I
    can tweak as we go.

---
+++ fish/functions/tmux-start.fish
@@ -0,0 +1,10 @@
+# Have consistent tmux start up
+function tmux-start
+    # TODO: Probably want to add something here to not create it immediately and check if there is another one existing
+    tmux new-session -d
+    tmux split-window -v
+    tmux split-window -h "change \"ultralist list group:project completed:false\" .todos.json"
+    tmux rename-window "jrnl"
+    tmux new-window
+    tmux attach-session -d
+end
