ab31956e7359532cbe6d52818ea59520aa466876
maccoda <maccora17@gmail.com>
2021-06-01 12:04:18 -0700

    Move change to a fish function.
    As part of this have also added a few options to it for clearing and
    adding the bell

---
+++ fish/functions/change.fish
@@ -0,0 +1,35 @@
+# Watches the provided directory (or 'src' if none provided)
+# and executes the given command when it changes
+#
+# -b/--bell: If set will call the bell on completion of the provided command
+# -c/--clear: If set will clear the terminal before each execution of the provided command
+function change_fish
+    argparse --min-args 1 'b/bell' 'c/clear' -- $argv
+    set cmd $argv[1]
+    set watch_dir $argv[2]
+    test -z $watch_dir; and set watch_dir 'src'
+    if test -z $cmd
+        echo "Usage: change cmd [watch_directory]"
+        return 1
+    end
+
+    if set -q _flag_c
+        clear
+    end
+    if set -q _flag_b
+        echo -e '\a'
+    end
+    eval $cmd
+    # TODO: Can make it so that the latency can be configurable if needed
+    fswatch -0 --latency 3 $watch_dir | while read -d "" event
+        echo "File Changed: $event"
+        if set -q _flag_c
+            clear
+
+        end
+        if set -q _flag_b
+            echo -e '\a'
+        end
+        eval $cmd
+    end
+end
--- fish/functions/tmux-start.fish
+++ fish/functions/tmux-start.fish
@@ -3,7 +3,7 @@ function tmux-start
     # TODO: Probably want to add something here to not create it immediately and check if there is another one existing
     tmux new-session -d
     tmux split-window -v
-    tmux split-window -h "change \"ultralist list group:project completed:false\" .todos.json"
+    tmux split-window -h "change \"ultralist list group:project completed:false\" .todos.json --clear"
     tmux rename-window "jrnl"
     tmux new-window
     tmux attach-session -d
