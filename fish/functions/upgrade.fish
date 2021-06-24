847ecd5ae0e01613fbbfbec0eefdedd8d4c76c5d
maccoda <maccora17@gmail.com>
2021-06-24 07:55:32 -0700

    Move upgrade scripts to autoload functions.
    This is just simply because this seems to be more idiomatic fish shell
    but it also adds the benefit of not having issues with the path to the
    fish binary which is actually different on the different laptops.

---
+++ fish/functions/upgrade.fish
@@ -0,0 +1,42 @@
+function upgrade
+    echo "Pulling updates for dotfiles..."
+    cd $HOME/.dotfiles
+    git submodule update --recursive --remote
+    gpr
+
+    # Update any links
+    ./install
+
+    switch (uname -s)
+
+       case Darwin
+          echo "Detected MacOS. Running MacOS specific managers..."
+
+       case Linux
+          echo "Detected Linux. Running Linux specific managers..."
+          sudo apt-get update -q
+          sudo apt-get upgrade -qy
+          sudo apt-get autoremove -qy
+          snap refresh
+
+       case '*'
+         echo 'Unknown OS'
+         exit 1
+    end
+
+    echo "Updating brew taps..."
+    brew upgrade
+    brew cleanup
+
+    echo "Updating fisher packages..."
+    fisher update
+
+    echo "Updating rust..."
+    rustup update
+
+    echo "Updating vim..."
+    nvim +PlugUpdate +qall
+    nvim +PlugClean +qall
+
+   work-upgrade
+end
--- fish/functions/work-upgrade.fish
+++ fish/functions/work-upgrade.fish
@@ -1,7 +1,6 @@
-#!/bin/bash
-
 # This file is executed by the upgrade script. It is for any work specific
 # commands do not want committed to dot files repository
+function work-upgrade
+    echo "Running work related upgrades"
 
-echo "Running work related upgrades"
-
+end
