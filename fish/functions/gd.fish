d24c5eb5bf27cadbdea4dcacbe0c3c931631610d
maccoda <maccora17@gmail.com>
2021-11-30 15:45:45 -0800

    Fix gd script to run from any location
    https://stackoverflow.com/questions/53675286/how-does-git-interpret-a-colon-followed-by-a-path
    
    The gd script preview was failing whenever it was run outside of the
    root directory. The reason for this was that the path was being given
    was relative to the root directory but when the preview command executed
    it was calling for a path that did not exist. From the above link we can
    prefix a pathspec with :/ and git will interpret that from the root
    directory, matching the expected format.

--- fish/functions/gd.fish
+++ fish/functions/gd.fish
@@ -1,6 +1,6 @@
 # Interactive git staging
 function gd
-    set -l preview "git diff $argv --color=always -- {-1}"
+    set -l preview "git diff $argv --color=always -- :/{-1}"
     git diff $argv --name-only | fzf -m --ansi --preview $preview \
     --bind "s:execute-silent(git add {})+reload(git diff $argv --name-only),p:execute(git add -p {}),ctrl-r:reload(git diff $argv --name-only)" \
     --header 'Press Ctrl+r to reload'
