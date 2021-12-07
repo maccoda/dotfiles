21bc6dc3edc779443e900383a2d3fb48b0ad6f93
maccoda <maccora17@gmail.com>
2021-12-07 11:36:26 -0800

    Update git fzf scripts to be even more useful!

--- fish/functions/commit.fish
+++ fish/functions/commit.fish
@@ -1,4 +1,11 @@
 # Usage: Search across commits with fzf
+# Can provide usual git log parameters to the initial call and will be used to initially filter the results
+# The fuzzy search will only be performed on the author and commit messages
 function commit
-    grg --color=always | fzf --preview='git show --color=always {1}' --ansi
+    set selected (grg --color=always $argv | fzf --preview='git show --color=always {1}' --ansi --no-sort -n 2,4..)
+    if test -z $selected
+        return
+    end
+    set short_hash (echo $selected | cut -d ' ' -f 1)
+    git show $short_hash
 end
