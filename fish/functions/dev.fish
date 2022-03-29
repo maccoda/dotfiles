b93d2c385c1f3bf88ee6bf00e41344f9217b6b94
Dylan Maccora <maccora17@gmail.com>
2022-03-29 11:38:56 +1100

    Improve formatting of dev pr command

--- fish/functions/dev.fish
+++ fish/functions/dev.fish
@@ -12,13 +12,13 @@ function dev
         # Check status of all pull requests
         for repo in (bat --plain ~/.config/dev-tools/following)
             cd $repo
-            set search_query "review:required draft:false"
+            set search_query "draft:false"
             set has_prs (gh pr list --search "$search_query" --json number --limit 1 --jq '.[] | length' | string trim)
             test -z $has_prs; and set has_prs 0
             if test $has_prs -gt 0
                 heading --no-trail (basename $repo)
-                gh pr list --search "$search_query" --json number,title,url --template \
-                    '{{range .}}{{tablerow (printf "#%v" .number | autocolor "green") .title .url}}{{end}}'
+                gh pr list --search "$search_query" --json number,title,url,author,reviewDecision --template \
+                    '{{range .}}{{tablerow (printf "#%v" .number | autocolor "green") (printf "@%v" .author.login | autocolor "blue") (truncate 60 .title) .reviewDecision  .url}}{{end}}'
             end
         end
         cd $start_dir
