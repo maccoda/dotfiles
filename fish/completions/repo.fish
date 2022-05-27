7874ef9ac5cf52dde1c7a60bfd5de6c3821cb680
Dylan Maccora <maccora17@gmail.com>
2022-05-27 18:01:24 +1000

    Improve the completions for the repo command
    Now this does not show the sub-commands if one has already been
    provided. It also offers the branch name when running the rebase
    command.

--- fish/completions/repo.fish
+++ fish/completions/repo.fish
@@ -1,3 +1,6 @@
 # Completions for repo command
-complete -c repo --arguments "feature-start init prune-branches follow rebase main"
+set -l commands feature-start init prune-branches follow rebase main
+complete -c repo --arguments "$commands" -n "not __fish_seen_subcommand_from $commands"
+complete -c repo --long-option "from-current" -n "__fish_seen_subcommand_from feature-start"
 complete -c repo --no-files
+complete -c repo -n "__fish_seen_subcommand_from rebase" -a "(git branch --format='%(refname:short)')"
