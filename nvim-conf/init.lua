92176830fa8db1f3d575f41a6f292873badbdf8f
Dylan Maccora <maccora17@gmail.com>
2022-11-08 21:42:19 +1100

    Add same keymaps for rust LSP

--- nvim-conf/init.lua
+++ nvim-conf/init.lua
@@ -386,6 +386,14 @@ for _, lsp in pairs(servers) do
     end
 end
 
+local rt = require("rust-tools")
+
+rt.setup({
+    server = {
+        on_attach = common_on_attach
+    },
+})
+
 
 
 -- == tree-sitter ==
@@ -447,18 +455,6 @@ require('neoscroll').setup()
 
 require("twilight").setup()
 
-local rt = require("rust-tools")
-
-rt.setup({
-    server = {
-        on_attach = function(_, bufnr)
-            -- Hover actions
-            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-            -- Code action groups
-            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-        end,
-    },
-})
 
 require("todo-comments").setup {
     signs = false
