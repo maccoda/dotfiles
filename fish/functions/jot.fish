cfb7fbcc1f71e13ef31f0ff9926622bc896fcaf4
Dylan Maccora <maccora17@gmail.com>
2023-01-11 17:17:57 +1100

    Allow jot command to write to tmp as an option

--- fish/functions/jot.fish
+++ fish/functions/jot.fish
@@ -1,7 +1,14 @@
 # Make a quick note
+#
+# -t/tmp: If set will make a temporary file
 function jot
-    mkdir -p $HOME/jot
+    argparse 't/tmp' -- $argv
     set stamp (date '+%Y-%m-%dT%H-%M-%S')
-    set file_path "$HOME/jot/jot-$stamp.txt"
+    if set -q _flag_t
+        set file_path "/tmp/jot-$stamp.txt"
+    else
+        mkdir -p $HOME/jot
+        set file_path "$HOME/jot/jot-$stamp.txt"
+    end
     nvim $file_path
 end
