641f01f2702e1482238fb5ad52ac44101e4ae692
Dylan Maccora <maccora17@gmail.com>
2023-02-13 15:11:02 +1100

    Add search ability to jot command

--- fish/functions/jot.fish
+++ fish/functions/jot.fish
@@ -2,13 +2,18 @@
 #
 # -t/tmp: If set will make a temporary file
 function jot
-    argparse 't/tmp' -- $argv
-    set stamp (date '+%Y-%m-%dT%H-%M-%S')
-    if set -q _flag_t
-        set file_path "/tmp/jot-$stamp.txt"
+    argparse --ignore-unknown t/tmp -- $argv
+    if count $argv > /dev/null
+        set search_term $argv
+        rg --files-with-matches --ignore-case $search_term $HOME/jot | fzbat > /dev/null
     else
-        mkdir -p $HOME/jot
-        set file_path "$HOME/jot/jot-$stamp.txt"
+        set stamp (date '+%Y-%m-%dT%H-%M-%S')
+        if set -q _flag_t
+            set file_path "/tmp/jot-$stamp.txt"
+        else
+            mkdir -p $HOME/jot
+            set file_path "$HOME/jot/jot-$stamp.txt"
+        end
+        nvim $file_path
     end
-    nvim $file_path
 end
