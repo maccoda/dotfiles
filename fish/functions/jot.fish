afb5dfe59cbb3ea6ac5f68afc5737a8f68531f25
Dylan Maccora <maccora17@gmail.com>
2024-01-08 16:21:54 +1100

    Improve the jot command for better note taking

--- fish/functions/jot.fish
+++ fish/functions/jot.fish
@@ -3,17 +3,23 @@
 # -t/tmp: If set will make a temporary file
 function jot
     argparse --ignore-unknown t/tmp -- $argv
-    if count $argv > /dev/null
+    if count $argv >/dev/null
         set search_term $argv
         rg --files-with-matches --ignore-case $search_term $HOME/jot | fzbat
     else
         set stamp (date '+%Y-%m-%dT%H-%M-%S')
+        set title (gum input --prompt "Title: ")
+        set file_title (echo $title | sed 's/ /-/g' | tr "[:upper:]" "[:lower:]")
         if set -q _flag_t
-            set file_path "/tmp/jot-$stamp.txt"
+            set file_path "/tmp/jot-$file_title-$stamp.md"
         else
             mkdir -p $HOME/jot
-            set file_path "$HOME/jot/jot-$stamp.txt"
+            set file_path "$HOME/jot/$file_title-$stamp.md"
         end
-        nvim $file_path
+        touch $file_path
+        echo -e "# $title\n" >>$file_path
+        echo $stamp >>$file_path
+        echo >>$file_path
+        $EDITOR $file_path
     end
 end
