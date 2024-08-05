b38a9a3b69f2ad90dba0a4e869632a732cf78bfa
Dylan Maccora <maccora17@gmail.com>
2024-08-05 17:59:57 +1000

    Fix colouring and single digit IDs in task complete

--- fish/functions/task-complete.fish
+++ fish/functions/task-complete.fish
@@ -1,7 +1,8 @@
 # Task complete
 function task-complete
-    set task_cmd "task rc.defaultwidth=0 rc.defaultheight=0 rc.verbose=nothing rc._forcecolor=on"
-    set task_id (eval $task_cmd | fzf --height=~100% --ansi --preview-window="90%" | cut -d ' ' -f 1 | string trim)
+    set task_cmd "task rc.defaultwidth=0 rc.defaultheight=0 rc.verbose=nothing"
+    set task_id (eval $task_cmd | fzf --height=~100% --ansi --preview-window="90%" | string trim | cut -d ' ' -f 1 )
+    echo $task_id
     if test -z $task_id
         echo "No task selected"
         return 0
