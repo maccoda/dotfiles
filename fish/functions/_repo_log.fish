8e988aa914dac1fad955478941d22d5857b97800
Dylan Maccora <maccora17@gmail.com>
2025-03-27 17:02:46 +1100

    Remove repo fish functions
    Starting to use my own extracted plugin now so that I can stabilise it and hopefully share it

--- fish/functions/_repo_log.fish
+++
@@ -1,9 +0,0 @@
-# Usage: Search across commits with fzf
-# Can provide usual git log parameters to the initial call and will be used to initially filter the results
-# The fuzzy search will only be performed on the author and commit messages
-function _repo_log
-    grg --color=always $argv | fzf --preview 'git show --color=always --stat {1}' --ansi --no-sort -n 2.. \
-        --bind "enter:execute(git show --color=always {1} | less -R)" \
-        --bind "?:preview(git show {1})" \
-        --header "? for expanded diff"
-end
--- fish/functions/grg.fish
+++ fish/functions/grg.fish
@@ -1,5 +1,5 @@
 # Git log formatted for better grepping
 function grg --wraps='git log --pretty="format:%C(yellow)%h%C(reset) %C(blue)%<(15)%an%C(reset) %C(magenta)%cs%C(reset) %s"' --description 'alias grg=git log --pretty="format:%C(yellow)%h%C(reset) %C(blue)%<(15)%an%C(reset) %C(magenta)%cs%C(reset) %s"'
-    git log --pretty="format:%C(yellow)%h%C(reset) %C(blue)%<(15)%an%C(reset) %C(magenta)%cs%C(reset) %s" $argv
+    git log --pretty="format:%C(yellow)%h%C(reset) %C(blue)%<(15)%an%C(reset) %C(magenta)%cs%C(reset) %s %C(red)%d%C(reset)" $argv
 
 end
