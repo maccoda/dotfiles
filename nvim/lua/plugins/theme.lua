14e271e522909f0b2e4afcbe3c0e0fcaf221b727
Dylan Maccora <the.maccoda@gmail.com>
2025-04-02 21:27:25 +1100

    Add flutter.nvim and update theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,6 +1,16 @@
 return {
 
-  { "ellisonleao/gruvbox.nvim" },
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      overrides = {
+        LspReferenceRead = { bg = "#3c3836", fg = "#fabd2f", bold = true },
+        LspReferenceText = { bg = "#3c3836", fg = "#fabd2f", bold = true },
+        LspReferenceWrite = { bg = "#d65d0e", fg = "#fabd2f", bold = true },
+      },
+      dim_inactive = true,
+    },
+  },
 
   -- Configure LazyVim to load gruvbox
   {
