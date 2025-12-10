cf38dc93dcfa8534bc038dbea6528f38be452829
Dylan Maccora <the.maccoda@gmail.com>
2025-12-11 01:03:05 +1100

    Replace oil with mini.files
    Essentially have the same functionality, with a slightly different UI
    and also means one less plugin.

--- nvim-kick/lua/plugins/mini.lua
+++ nvim-kick/lua/plugins/mini.lua
@@ -74,15 +74,26 @@ return {
       }
 
       local starter = require 'mini.starter'
+      local header = function()
+        local hour = tonumber(vim.fn.strftime '%H')
+        -- [04:00, 12:00) - morning, [12:00, 20:00) - day, [20:00, 04:00) - evening
+        local part_id = math.floor((hour + 4) / 8) + 1
+        local day_part = ({ 'evening', 'morning', 'afternoon', 'evening' })[part_id]
+        local username = vim.loop.os_get_passwd()['username'] or 'USERNAME'
+        local cwd = vim.fn.getcwd()
+
+        return ('Good %s, %s\ncwd: %s'):format(day_part, username, cwd)
+      end
       starter.setup {
         evaluate_single = true,
         items = {
           { name = 'Find files', action = 'lua require("fzf-lua").files()', section = 'Picker' },
           { name = 'Grep files', action = 'lua require("fzf-lua").live_grep()', section = 'Picker' },
-          { name = 'Oil files', action = 'Oil', section = 'Picker' },
+          { name = 'Explore files', action = 'lua require("mini.files").open()', section = 'Picker' },
           starter.sections.recent_files(10, true),
           starter.sections.builtin_actions(),
         },
+        header = header(),
       }
 
       require('mini.bufremove').setup()
@@ -93,6 +104,42 @@ return {
         conflict = { suffix = 'n' },
         diagnostic = { options = { float = false } },
       }
+
+      require('mini.files').setup()
+      local map_split = function(buf_id, lhs, direction)
+        local rhs = function()
+          -- Make new window and set it as target
+          local cur_target = MiniFiles.get_explorer_state().target_window
+          local new_target = vim.api.nvim_win_call(cur_target, function()
+            vim.cmd(direction .. ' split')
+            return vim.api.nvim_get_current_win()
+          end)
+
+          MiniFiles.set_target_window(new_target)
+          MiniFiles.go_in { close_on_file = true }
+        end
+
+        -- Adding `desc` will result into `show_help` entries
+        local desc = 'Split ' .. direction
+        vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
+      end
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'MiniFilesBufferCreate',
+        callback = function(args)
+          local buf_id = args.data.buf_id
+          map_split(buf_id, '<C-s>', 'belowright horizontal')
+          map_split(buf_id, '<C-v>', 'belowright vertical')
+          map_split(buf_id, '<C-t>', 'tab')
+        end,
+      })
+
+      vim.keymap.set('n', '<leader>fe', function()
+        MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+      end, { desc = 'Explore parent directory' })
+      vim.keymap.set('n', '<leader>fE', function()
+        MiniFiles.open(nil, false)
+      end, { desc = 'Explore current working directory' })
     end,
   },
 }
